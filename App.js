a5993e966f6d67cbef211b7162b178f4088b425b
condrad26 <condradotim199@gmail.com>
2025-02-11 18:11:41 +0300

    update

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Transactions" component={TransactionScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />  
-          <Stack.Screen name="Sidebar" component={Sidebar} />  
+          //<Stack.Screen name="Sidebar" component={Sidebar} />  
           <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />  
         </Stack.Navigator>
       </NavigationContainer>
