8032e5f9f33354cb45605f45c97e8a2372516318
condrad26 <condradotim199@gmail.com>
2025-04-03 15:28:22 +0300

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { ThemeProvider } from './context/ThemeContext';
-import MainLayout from './components/MainLayout'; // Import the MainLayout component
+import MainLayout from './components/MainLayout';
 
 // Auth Screens
 import LandingScreen from './screens/LandingScreen';
@@ -23,63 +23,49 @@ import BudgetReportScreen from './screens/BudgetReportScreen';
 import UsersScreen from './screens/UsersScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
-const Stack = createStackNavigator();
+const RootStack = createStackNavigator();
+const MainAppStack = createStackNavigator();
 
-// Separate stack for authenticated screens with sidebar
-const AppStack = () => {
-  return (
-    <MainLayout>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="Transactions" component={TransactionScreen} />
-        <Stack.Screen name="Budget" component={BudgetScreen} />
-        <Stack.Screen name="Departments" component={DepartmentScreen} />
-        <Stack.Screen name="Accounts" component={AccountScreen} />
-        <Stack.Screen name="FinancialReport" component={FinancialReportScreen} />
-        <Stack.Screen name="BudgetReport" component={BudgetReportScreen} />
-        <Stack.Screen name="Users" component={UsersScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </MainLayout>
-  );
-};
+// Main App Stack with Sidebar
+const MainApp = () => (
+  <MainLayout>
+    <MainAppStack.Navigator 
+      initialRouteName="Home" 
+      screenOptions={{ headerShown: false }}
+    >
+      <MainAppStack.Screen name="Home" component={HomeScreen} />
+      <MainAppStack.Screen name="Transactions" component={TransactionScreen} />
+      <MainAppStack.Screen name="Budget" component={BudgetScreen} />
+      <MainAppStack.Screen name="Departments" component={DepartmentScreen} />
+      <MainAppStack.Screen name="Accounts" component={AccountScreen} />
+      <MainAppStack.Screen name="FinancialReport" component={FinancialReportScreen} />
+      <MainAppStack.Screen name="BudgetReport" component={BudgetReportScreen} />
+      <MainAppStack.Screen name="Users" component={UsersScreen} />
+      <MainAppStack.Screen name="Settings" component={SettingsScreen} />
+    </MainAppStack.Navigator>
+  </MainLayout>
+);
 
-// Main app navigation with auth flow
 export default function App() {
+  // Set this to false to bypass authentication
+  const [isAuthenticated, setIsAuthenticated] = React.useState(true); 
+
   return (
     <ThemeProvider>
       <PaperProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Landing">
-            {/* Auth Screens */}
-            <Stack.Screen
-              name="Landing"
-              component={LandingScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="SignUp"
-              component={SignUpScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="ForgotPassword"
-              component={ForgotPasswordScreen}
-              options={{ headerShown: false }}
-            />
-            
-            {/* Main App with Sidebar */}
-            <Stack.Screen
-              name="App"
-              component={AppStack}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
+          <RootStack.Navigator screenOptions={{ headerShown: false }}>
+            {isAuthenticated ? (
+              <RootStack.Screen name="MainApp" component={MainApp} />
+            ) : (
+              <>
+                <RootStack.Screen name="Landing" component={LandingScreen} />
+                <RootStack.Screen name="Login" component={LoginScreen} />
+                <RootStack.Screen name="SignUp" component={SignUpScreen} />
+                <RootStack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+              </>
+            )}
+          </RootStack.Navigator>
         </NavigationContainer>
       </PaperProvider>
     </ThemeProvider>
